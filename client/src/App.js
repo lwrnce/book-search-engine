d7b0ac6c3ca7a19237adede291a096f8e857c68f
lwrnce <lwrnceyuen@gmail.com>
2021-07-18 16:43:58 -0700

    fix app and page files for build deployment

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
 
 import { useQuery, useMutation } from "@apollo/react-hooks";
